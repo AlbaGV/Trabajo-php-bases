6e9631359559d5a6aa35e78311868c3ebde738d1
Alba GV <ciuxgokudo@hotmail.es>
2017-01-04 10:50:06 +0100

    Add files via upload

---
+++ residenciacanina.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 04-01-2017 a las 10:47:05
+-- Versión del servidor: 5.7.16-0ubuntu0.16.04.1
+-- Versión de PHP: 7.0.8-0ubuntu0.16.04.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `residenciacanina`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pass`
+--
+
+CREATE TABLE `pass` (
+  `usuario` varchar(20) COLLATE utf8_bin NOT NULL,
+  `password` varchar(20) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `pass`
+--
+
+INSERT INTO `pass` (`usuario`, `password`) VALUES
+('ciux', 'bleach69'),
+('zombie_wolf', 'jester69');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `perros`
+--
+
+CREATE TABLE `perros` (
+  `codigo` int(10) NOT NULL,
+  `nombre` varchar(30) COLLATE utf8_bin NOT NULL,
+  `raza` varchar(50) COLLATE utf8_bin DEFAULT NULL,
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dueño` varchar(50) COLLATE utf8_bin NOT NULL,
+  `telefono` int(15) NOT NULL,
+  `correo` varchar(50) COLLATE utf8_bin DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `perros`
+--
+
+INSERT INTO `perros` (`codigo`, `nombre`, `raza`, `fecha`, `dueño`, `telefono`, `correo`) VALUES
+(1, 'Kiba', 'Pastor Aleman', '2016-11-15 23:00:00', 'Carlos', 546186165, 'ejemplo@gmail.com'),
+(2, 'Simba', '', '2016-11-17 07:27:29', 'Robert', 84112861, ''),
+(3, 'Luna', 'Husky', '2016-11-16 18:52:07', 'Carmen', 65961861, ''),
+(44, 'Zambo', '', '2016-11-17 07:58:43', 'Alba Garcia', 697979084, 'ohallo@gmail.es');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `pass`
+--
+ALTER TABLE `pass`
+  ADD PRIMARY KEY (`usuario`),
+  ADD UNIQUE KEY `usuario` (`usuario`);
+
+--
+-- Indices de la tabla `perros`
+--
+ALTER TABLE `perros`
+  ADD PRIMARY KEY (`codigo`),
+  ADD UNIQUE KEY `codigo` (`codigo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `perros`
+--
+ALTER TABLE `perros`
+  MODIFY `codigo` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
